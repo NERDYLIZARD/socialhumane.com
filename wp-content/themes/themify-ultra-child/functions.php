b84b28807d494349963b4176dd675a5b6487edd1
NERDYLIZARD <bouasavanhhop@gmail.com>
2017-08-05 00:19:35 +0530

    config gallery-thumb & remove builder for editor

--- wp-content/themes/themify-ultra-child/functions.php
+++ wp-content/themes/themify-ultra-child/functions.php
@@ -1,3 +1,8 @@
 <?php
 
 // additional function
+add_theme_support( 'post-thumbnails' );
+
+add_image_size( 'sidebar-feature-post-thumb', 120, 68 );  
+add_image_size( 'profile-picture', 250, 250 );  
+add_image_size( 'gallery-thumb', 400, 400 );  
