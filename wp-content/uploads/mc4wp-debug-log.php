5d5c3ec883a225795b54dd36d0039d4f008d2373
nerdylizard <bouasavanhhop@gmail.com>
2017-08-05 08:26:31 +0000

    from production 05082017

--- wp-content/uploads/mc4wp-debug-log.php
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -1,2 +1,6 @@
 <?php exit; ?>
 [2017-07-25 12:57:43] WARNING: Form 62 > boua*********@gm***.com is already subscribed to the selected list(s)
+[2017-08-05 10:38:46] WARNING: Form 62 > boua*********@gm***.com is already subscribed to the selected list(s)
+[2017-08-05 10:40:50] WARNING: Form 62 > boua*********@gm***.com is already subscribed to the selected list(s)
+[2017-08-05 10:41:16] WARNING: Form 62 > boua*********@gm***.com is already subscribed to the selected list(s)
+[2017-08-05 10:41:58] ERROR: Contact Form 7 > MailChimp API Error: cURL error 28: Connection timed out after 10000 milliseconds.
