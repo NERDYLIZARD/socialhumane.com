195ceb2db836ba43af39efa3b2825689df8a2404
nerdylizard <bouasavanhhop@gmail.com>
2017-08-14 09:32:16 +0000

    modify 'continue reading'

--- wp-content/themes/themify-ultra-child/functions.php
+++ wp-content/themes/themify-ultra-child/functions.php
@@ -1,8 +1,20 @@
 <?php
 
-// additional function
+/**
+ * Custom image size
+ */
 add_theme_support( 'post-thumbnails' );
 
 add_image_size( 'sidebar-feature-post-thumb', 120, 68 );  
 add_image_size( 'profile-picture', 250, 250 );  
 add_image_size( 'gallery-thumb', 480, 480 );  
+
+
+/**
+ * Replaces the excerpt "more" text by a link.
+ */
+function new_excerpt_more($more) {
+  global $post;
+  return '... <a class="more-tag" href="'. get_permalink($post->ID) . '"> continue reading &raquo;</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
--- wp-content/uploads/mc4wp-debug-log.php
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -6,3 +6,4 @@
 [2017-08-05 10:41:58] ERROR: Contact Form 7 > MailChimp API Error: cURL error 28: Connection timed out after 10000 milliseconds.
 [2017-08-10 10:07:08] ERROR: Form 62 > MailChimp API error: 0 cURL error 28: Operation timed out after 10000 milliseconds with 0 bytes received.
 [2017-08-10 23:14:35] ERROR: Form 62 > MailChimp API error: 400 Bad Request. dipi***********@fa******.com looks fake or invalid, please enter a real email address.
+[2017-08-13 16:58:50] WARNING: Form 62 > boua*********@gm***.com is already subscribed to the selected list(s)
